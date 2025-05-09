87f0ab9b0864edc21071ee08eee1125f4aeae8d3
SachinYadav113 <106666847+SachinYadav113@users.noreply.github.com>
2025-05-09 16:28:22 +0530

    Update HelloUtil.java

--- src/main/java/com/example/utility/HelloUtil.java
+++ src/main/java/com/example/utility/HelloUtil.java
@@ -9,7 +9,7 @@ package com.example.utility;
 
 public class HelloUtil {
     public static String getMessage() {
-        return "Hello from utility-lib!";
+        return "Hello from utility-lib!testing";
     }
 }
 
